3e9c213e450a3a199c8de1714e78607a361199e5
Stephen Bush <trininox@gmail.com>
2011-04-22 20:18:57 -0400

    Version 0.4.22

---
+++ operators/function_connect.php
@@ -0,0 +1,14 @@
+<!--
+ - PHP Document
+ - Copyright 2011 
+ - Stephen Bush 
+ - trininox@gmail.com 
+ - Version 0.4.21
+ - Includes jQuery.js
+-->
+<?php	
+	date_default_timezone_set('America/Fort_Wayne');
+	mysql_connect("localhost","root",""); 	
+	mysql_select_db("eas_reports");
+	
+?>
